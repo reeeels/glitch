88dfcb00016ae8d4f393fcb2605568dcbe93ee47
Glitch (hello-webpage) <none>
2021-02-24 17:14:40 +0000

    🙋🛤 Checkpoint ./script.js:29012282/1126 ./index.html:29012282/206 ./style.css:29012282/62

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 var pattern = [2, 2, 4, 3, 2, 1, 2, 4];
 var progress = 0;
 var gameplaying = false;
+var tonePlaying = false;
+var volume = 0.3;
 
 function startGame(){
   //initialize game variables
@@ -19,5 +21,50 @@ function stopGame(){
   //swap the Start and Stop buttons
   document.getElementById("startBtn").classList.remove("hidden");
   document.getElementById("endBtn").classList.add("hidden");
+}
+
+// Sound Synthesis Functions
+const freqMap = {
+  1: 271.6,
+  2: 309.6,
+  3: 380,
+  4: 450.2
+}
+function playTone(btn,len){ 
+  o.frequency.value = freqMap[btn]
+  g.gain.setTargetAtTime(volume,context.currentTime + 0.05,0.025)
+  tonePlaying = true
+  setTimeout(function(){
+    stopTone()
+  },len)
+}
+function startTone(btn){
+  if(!tonePlaying){
+    o.frequency.value = freqMap[btn]
+    g.gain.setTargetAtTime(volume,context.currentTime + 0.05,0.025)
+    tonePlaying = true
+  }
+}
+function stopTone(){
+    g.gain.setTargetAtTime(0,context.currentTime + 0.05,0.025)
+    tonePlaying = false
+}
+
+//Page Initialization
+// Init Sound Synthesizer
+var context = new AudioContext()
+var o = context.createOscillator()
+var g = context.createGain()
+g.connect(context.destination)
+g.gain.setValueAtTime(0,context.currentTime)
+o.connect(g)
+o.start(0)
+
+function lightButton(btn){
+  document.getElementById("button"+btn).classList.add("lit")
+}
+
+function lightButton(btn){
+  document.getElementById("button"+btn).classList.remove("lit")
 }
 
