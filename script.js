b8a2a1ce9a924213cb53c9e6fa3b3a8448a99340
Glitch (hello-webpage) <none>
2021-02-24 17:03:04 +0000

    🌸🎥 Checkpoint ./style.css:29012282/168 ./script.js:29012282/909 ./index.html:29012282/55

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+//Global Variables
+var pattern = [2, 2, 4, 3, 2, 1, 2, 4];
+var progress = 0;
+var gameplaying = false;
 
-// prints "hi" in the browser's dev tools console
-console.log("hi");
+function startGame(){
+  //initialize game variables
+  progress = 0;
+  gamePlaying = true;
+  
+  //swap the Start and Stop buttons
+  document.getElementById("startBtn").classList.add("hidden");
+  document.getElementById("endBtn").classList.remove("hidden");
+}
+
+function stopGame(){
+  gamePlaying = false;
+  
+  //swap the Start and Stop buttons
+  document.getElementById("startBtn").classList.remove("hidden");
+  document.getElementById("endBtn").classList.add("hidden");
+}
+
